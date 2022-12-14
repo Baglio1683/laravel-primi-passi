cf873ee35833489d9b761bdd3e1cc9cff4224e40
MyUsername <alberto.baglivi@gmail.com>
2022-12-14 16:09:58 +0100

    Add Hello World Dinamic

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+
+    $Hello = "Hello World Dinamic"; 
+    return view('home', compact('Hello'));
+
 });
